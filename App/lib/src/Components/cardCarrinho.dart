b29f7c543c30ec875074d112357fa7c4df3b5e12
Israel Oliveira <israelolieira.dev@gmail.com>
2024-01-05 23:59:40 -0300

    finalizando carrinho

--- App/lib/src/Components/cardCarrinho.dart
+++ App/lib/src/Components/cardCarrinho.dart
@@ -10,6 +10,97 @@ class CardCarrinho extends StatefulWidget {
 class _CardCarrinhoState extends State<CardCarrinho> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(color: Color(0xFFD3D3D3), width: 2),
+                  borderRadius: BorderRadius.circular(10.0)),
+              width: 45,
+              height: 73,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Column(
+                  children: [
+                    InkWell(
+                      onTap: () {},
+                      child: Icon(
+                        Icons.keyboard_arrow_up,
+                        color: Color(0xFFD3D3D3),
+                      ),
+                    ),
+                    Text(
+                      "0",
+                      style: TextStyle(fontSize: 18, color: Colors.grey),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Icon(
+                        Icons.keyboard_arrow_down,
+                        color: Color(0xFFD3D3D3),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 15,
+            ),
+            Container(
+              height: 70,
+              width: 70,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/pastel.png"),
+                    fit: BoxFit.cover),
+                borderRadius: BorderRadius.circular(35),
+                // boxShadow: [
+                //   BoxShadow(
+                //       color: Colors.black,
+                //       blurRadius: 5.0,
+                //       offset: Offset(0, 2)
+                //       )
+                // ]
+              ),
+            ),
+            SizedBox(
+              width: 15,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Pastel de Flango",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  "15,00",
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.orangeAccent,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            Spacer(),
+            IconButton(onPressed: () {}, icon: Icon(Icons.close))
+          ],
+        ),
+      ),
+    );
   }
 }
+
+/*
+
+*/
--- App/lib/src/Pages/carrinho_pages.dart
+++ App/lib/src/Pages/carrinho_pages.dart
@@ -19,8 +19,103 @@ class _CarrinhoPageState extends State<CarrinhoPage> {
         scrollDirection: Axis.vertical,
         children: [
           CardCarrinho(),
+          CardCarrinho(),
+          CardCarrinho(),
+          CardCarrinho(),
         ],
       ),
+      bottomNavigationBar: _totalContainer(),
+    );
+  }
+
+  Widget _totalContainer() {
+    return Container(
+      height: 190,
+      padding: EdgeInsets.only(left: 10, right: 10),
+      margin: EdgeInsets.only(top: 30),
+      child: Column(children: [
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Subtotal:",
+              style: TextStyle(
+                  color: Color(0xFF9BA7C6),
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "23,00",
+              style: TextStyle(
+                  color: Color(0xFF6C6D6D),
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Taxa de Entrega:",
+              style: TextStyle(
+                  color: Color(0xFF9BA7C6),
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "3,00",
+              style: TextStyle(
+                  color: Color(0xFF6C6D6D),
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Divider(
+          height: 2,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Total:",
+              style: TextStyle(
+                  color: Color(0xFF9BA7C6),
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "26,00",
+              style: TextStyle(
+                  color: Color(0xFF6C6D6D),
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        ElevatedButton(
+            onPressed: () {},
+            child: Container(
+              height: 50,
+              child: Center(child: Text("FInalizar Pagamento")),
+            ))
+      ]),
     );
   }
 }
